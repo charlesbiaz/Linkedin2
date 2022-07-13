88df031854d4c14778a3bd651dc7bff05bf8ffff
Charles Biaz <charles.biaz@gmail.com>
2022-07-13 12:34:29 +0200

    Initial Commit

---
+++ Linkedin2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Linkedin2
+//
+//  Created by Charles Biaz on 13/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
